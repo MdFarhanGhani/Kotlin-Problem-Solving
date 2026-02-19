f8a15cd3d638b4966a8559ca4c79d6f838b60f77
MD FARHAN GHANI <128183288+MdFarhanGhani@users.noreply.github.com>
2026-02-19 15:27:10 +0530

    Create LC_02_printVariable.kt

---
+++ Variable/LC_02_printVariable.kt
@@ -0,0 +1,16 @@
+/*
+Write a Kotlin program that stores the name of a person in a variable and prints a birthday greeting message for him/her.
+
+You have to print the following in 2 separate lines.
+
+“Hello Surya”
+
+“Happy birthday to you”*/
+
+fun main(){
+    val name: String = "Surya"
+
+    println("Hello,$name!")
+    println("Happy birthday to you")
+
+}
